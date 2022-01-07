2dc78d2d0209bb91ad75f8d89af5c02c5812a457
William Warner Burke <williamburke121@gmail.com>
2022-01-06 21:51:26 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    willy: "12 Broadway"
+
+};
+
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, {[key]: value}); 
+    
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+employee[key]=value;
+return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmp= Object.assign({}, employee)
+    delete newEmp[key]
+    return newEmp
+    
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+
+}
